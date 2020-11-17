80a534de8c0de3f300572aa46e98b5b0431ec9e4
Renata Abreu <renata89abreu@gmail.com>
2020-11-17 16:42:37 -0300

    code formatting

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -17,20 +17,17 @@ const updateDatabase = (request, response) => {
     listFolderFiles(PATH, (file) => {
         const id = md5File.sync(file)
         console.log(`${id} ${file}`)
-        songCollection.findById(id, (error, song) => {
+        songCollection.findById(id, async (error, song) => {
             if (error)
                 console.log(error)
             if (!song) {
-                const saveSong = async (file) => {
-                    const songObj = new Song(file)
-                    await songObj.fillWithMetadata()
-                    new songCollection(songObj)
-                        .save((error) => {
-                            if (error)
-                                console.error(error)
-                        })
-                }
-                saveSong(file)
+                const songObj = new Song(file)
+                await songObj.fillWithMetadata()
+                new songCollection(songObj)
+                    .save((error) => {
+                        if (error)
+                            console.error(error)
+                    })
             } else {
                 if (song.filepath != file) {
                     songCollection.findByIdAndUpdate(song._id, { "filepath": file }, (error) => {
@@ -42,21 +39,21 @@ const updateDatabase = (request, response) => {
         })
     })
     songCollection.find((error, songs) => {
-        if(error)
-            console.error(error) 
-        else  
+        if (error)
+            console.error(error)
+        else
             songs.map(song => {
-                if(!fs.existsSync(song.filepath))
+                if (!fs.existsSync(song.filepath))
                     songCollection.findByIdAndUpdate(song._id, { "orphan": true }, (error) => {
-                        if(error)
+                        if (error)
                             console.error(error)
                     })
                 else
                     songCollection.findByIdAndUpdate(song._id, { "orphan": false }, (error) => {
-                        if(error)
+                        if (error)
                             console.error(error)
                     })
-                
+
             })
     })
     return response.status(200).send({
