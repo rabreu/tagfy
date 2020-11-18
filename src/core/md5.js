67ee3c2cf10b71402a09832f858f8413d8db2e25
Renata Abreu <renata89abreu@gmail.com>
2020-11-18 09:05:56 -0300

    md5.js not needed anymore

--- src/core/md5.js
+++
@@ -1,9 +0,0 @@
-const md5File = require('md5-file')
-
-const md5 = (files) => {
-    md5Files = []
-    files.forEach(file => {
-        md5Files.push(md5File(file))
-    });
-    return md5Files
-}
