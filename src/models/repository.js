6f1475b4eae40bd678f44a1a21fa71ed85a71847
Renata Abreu <renata89abreu@gmail.com>
2020-11-20 13:41:32 -0300

    connection message updated

--- src/models/repository.js
+++ src/models/repository.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 const { DATABASE_HOSTNAME, DATABASE_PORT, DATABASE_NAME } = require('../conf')
 
+const URL = `mongodb://${DATABASE_HOSTNAME}:${DATABASE_PORT}/${DATABASE_NAME}`
+
 const connect = () => {
-    mongoose.connect(`mongodb://${DATABASE_HOSTNAME}:${DATABASE_PORT}/${DATABASE_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     const connection = mongoose.connection
     connection.on("error", () => console.log("Couldn't connect to MongoDB."))
-    connection.once("open", () => console.log("Connected to  MongoDB."))
+    connection.once("open", () => console.log(`Connected to MongoDB on ${URL}.`))
 }
 
 module.exports = { connect }
